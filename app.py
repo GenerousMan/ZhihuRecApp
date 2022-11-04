4eb78cd6cbddecf0912317bbff41c2f875fb9417
zhoumingxu180 <1120927691@qq.com>
2022-11-04 15:35:42 +0800

    添加fetch_answer_boundTopicIDs

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ from flask import request
 from flask_cors import *
 import flask
 
-from tools.io import read_answer
+from tools.io import read_answer_text
+from tools.io import read_answer_boundTopicIDs
 from tools.tfidf import TFIDFSimilarity
 
 #创建应用程序
@@ -18,17 +19,20 @@ model_path = "./model/"
 tf_simi = TFIDFSimilarity()
 tf_simi.load_tfidf(model_path)
 
-# 写一个函数来处理浏览器发送过来的请求
-@app.route("/test", methods=['POST'])     #当访问网址时，默认执行下面函数
-def index():
-    return 'weclome to flask!!!'
 
-@app.route("/fetch_answer", methods=['POST'])
-def fetch_answer():
+@app.route("/fetch_answer_text", methods=['POST'])
+def fetch_answer_text():
     # 输入answer_ID,获取指定answer id的文本信息
     answer_id = request.get_json().get('answer_ID')
-    text = read_answer(answer_id)
+    text = read_answer_text(answer_id)
     print(text)
+    return text
+
+@app.route("/fetch_answer_boundTopicIDs", methods=['POST'])
+def fetch_answer_boundTopicIDs():
+    # 输入answer_ID,获取指定answer id绑定的话题ID列表数组
+    answer_id = request.get_json().get('answer_ID')
+    text = read_answer_boundTopicIDs(answer_id)
     return text
 
 @app.route("/compare_id", methods=['POST'])
@@ -37,12 +41,11 @@ def compare_id():
     # 输入两个answer_ID,获取两个回答的文本相似度
     answer_id_a = request.get_json().get('ID_a')
     answer_id_b = request.get_json().get('ID_b')
-    text_a = read_answer(answer_id_a)
-    text_b = read_answer(answer_id_b)
-    simi = tf_simi.compare_similarity(text_a, text_b, 0.3)
+    text_a = read_answer_text(answer_id_a)
+    text_b = read_answer_text(answer_id_b)
+    simi = tf_simi.compare_similarity(text_a, text_b, 0.33)
 
-    return json.dumps(simi)
-    #return text_b
+    return str(simi)
 
 @app.route("/compare_text", methods=['POST'])
 @cross_origin()
@@ -50,19 +53,25 @@ def compare_text():
     # 输入两个文本,获取相似度
     text_a = request.get_json().get('text_a')
     text_b = request.get_json().get('text_b')
-    simi = tf_simi.compare_similarity(text_a, text_b, 0.3)
+    simi = tf_simi.compare_similarity(text_a, text_b, 0.33)
 
     return str(simi)
 
-@app.route("/roc")     #路由功能
-def index1():
-    #这里处理业务逻辑
-    return '欢迎你!!!'
+@app.route("/get_text_characteristic_value", methods=['POST'])
+@cross_origin()
+def get_text_characteristic_value():
+    # 输入原始文本，返回经tfidf处理后按照特征值由高到低排序的列表数组（去重）
+    original_text = request.get_json().get('text')
+    result = tf_simi.text_2_tfidf_characteristic_value(original_text)
 
-@app.route("/roc/qq")
-def index2():
-    #这里处理业务逻辑
-    return 'hahaha!!!'
+    return result
+
+@app.route("/fetch_user_interaction_answers", methods=['POST'])
+@cross_origin()
+def fetch_user_interaction_answers():
+    # 输入用户的ID，返回展示给该用户的回答信息列表以及该用户阅读的回答信息列表
+    user_id = request.get_json().get('user_ID')
+    return 'abcd'
 
 if __name__ == "__main__":
     app.run(port=5050, debug=True)    #启动应用程序
--- tools/tfidf.py
+++ tools/tfidf.py
@@ -40,15 +40,27 @@ class TFIDFSimilarity:
         tf_kw_a = self.tfidf[kw_vector_a]
         tf_kw_a.sort(key = lambda x : x[1], reverse=True)
         a_list = [pair[0] for pair in tf_kw_a]
-        a_list = a_list[:int(max_thres*len(a_list))]
+        #文本在20个字符以内则全保留
+        a_list = a_list[:] if len(a_list) <= 20 else a_list[:int(max_thres*len(a_list))]
+        #a_list = a_list[:int(max_thres*len(a_list))]
 
         kw_vector_b = self.dictionary.doc2bow(string_b.split(" "))
         tf_kw_b = self.tfidf[kw_vector_b]
         tf_kw_b.sort(key=lambda x: x[1], reverse=True)
         b_list = [pair[0] for pair in tf_kw_b]
-        b_list = b_list[:int(max_thres * len(b_list))]
+        b_list = b_list[:] if len(b_list) <= 20 else b_list[:int(max_thres*len(b_list))]
+        #b_list = b_list[:int(max_thres * len(b_list))]
 
         j_simi = self.jaccard(a_list,b_list)
         print(j_simi)
         return j_simi
+
+    # 输入原始文本，返回经tfidf处理后按照特征值映射（与原字不同）由高到低排序的列表数组（去重）
+    def text_2_tfidf_characteristic_value(self, original_string):
+        kw_vector = self.dictionary.doc2bow(original_string.split(" "))
+        tf_kw = self.tfidf[kw_vector]
+        tf_kw.sort(key= lambda x : x[1], reverse=True)
+        result = [pair[0] for pair in tf_kw]
+
+        return result
 
